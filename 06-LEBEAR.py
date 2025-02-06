930bcbda18765029cc532e14673cd8694f9802c6
Luisa Mello <47289221+luisaddm@users.noreply.github.com>
2025-02-06 17:19:43 -0300

    Add files via upload

--- 06-LEBEAR.py
+++ 06-LEBEAR.py
@@ -5,7 +5,7 @@ aerosol optical depth values measured with the lidar and the sun-photometer
 Created on Sat Feb  5 07:51:19 2022
 @author: Fábio J. S. Lopes
 """
- 
+
 import os
 
 import numpy as np
@@ -16,13 +16,14 @@ from functions import milgrau_function as mf
 from lidar_retrievals import glue, kfs, retrieval_plots
 from molecular import lidarmolfit as lmfit
 
+from datetime import datetime
+
 """initial setup"""
 rootdir_name = os.getcwd()
 files_dir_level1 = "05-data_level1"
 files_dir_to_read = "02-preprocessed_corrected"
 rawinsonde_folder = "07-rawinsonde"
 datadir_name = os.path.join(rootdir_name, files_dir_level1)
-
 
 """flag to calculate molecular profile """
 
@@ -38,60 +39,68 @@ optical_prop_scale = 1e6  # optical properties graphics unit in Mega-meter
 altitude_scale = 1000  # altitude scale in km (a.g.l.)
 altitude_min = 0.0  # minimum altitude range for bacckscatter and extinction graphics
 altitude_max = 30  # minimum altitude range for bacckscatter and extinction graphics
+save_plots = True
 
 
-lraerosol_mean = {'355':{
-'01':	75.95179420295759,
-'02':	116.0781281327432,
-'03':	93.00495168575227,
-'04':	96.04858083333747,
-'05':	81.16349118630502,
-'06':	78.26140169693433,
-'07':	78.20827262376653,
-'08':	81.14378469642025,
-'09':	84.68682233680514,
-'10':	84.87031582521946,
-'11':	91.06223890036068,
-'12':	99.3530907245606 },
-                '532':{
-'01':	58.10536753439628,
-'02':	66.06093874128817,
-'03':	61.93402744043742,
-'04':	69.49695351083719,
-'05':	62.06715278749705,
-'06':	61.72606447952922,
-'07':	58.88385500748774,
-'08':	55.05473648423215,
-'09':	55.78110226421323,
-'10':	57.83180290771037,
-'11':	57.11318566352885,
-'12':	75.16787397694915},
-                '1064':{
-'01':	35.93783019664728,
-'02':	25.519609750457224,
-'03':	35.37311522020933,
-'04':	40.763400519628036,
-'05':	39.80843067334443,
-'06':	43.757009331438276,
-'07':	36.91946966241792,
-'08':	30.85041991885892,
-'09':	29.744870737420083,
-'10':	33.443201948631724,
-'11':	30.8290633146269,
-'12':	41.432981739001136,}
-                }
+lraerosol_mean = {
+    "355": {
+        "01": 75.95179420295759,
+        "02": 116.0781281327432,
+        "03": 93.00495168575227,
+        "04": 96.04858083333747,
+        "05": 81.16349118630502,
+        "06": 78.26140169693433,
+        "07": 78.20827262376653,
+        "08": 81.14378469642025,
+        "09": 84.68682233680514,
+        "10": 84.87031582521946,
+        "11": 91.06223890036068,
+        "12": 99.3530907245606,
+    },
+    "532": {
+        "01": 58.10536753439628,
+        "02": 66.06093874128817,
+        "03": 61.93402744043742,
+        "04": 69.49695351083719,
+        "05": 62.06715278749705,
+        "06": 61.72606447952922,
+        "07": 58.88385500748774,
+        "08": 55.05473648423215,
+        "09": 55.78110226421323,
+        "10": 57.83180290771037,
+        "11": 57.11318566352885,
+        "12": 75.16787397694915,
+    },
+    "1064": {
+        "01": 35.93783019664728,
+        "02": 25.519609750457224,
+        "03": 35.37311522020933,
+        "04": 40.763400519628036,
+        "05": 39.80843067334443,
+        "06": 43.757009331438276,
+        "07": 36.91946966241792,
+        "08": 30.85041991885892,
+        "09": 29.744870737420083,
+        "10": 33.443201948631724,
+        "11": 30.8290633146269,
+        "12": 41.432981739001136,
+    },
+}
 
 fileinfo, subfolderinfo = mf.readfiles_meastype(datadir_name)
-month=fileinfo[0][-6:-4]
-lraerosol = lraerosol_mean[str(lamb)][month]
-
 
 altitude_min_01 = 0  # minimum altitude range for scattering ratio graphic 01
 altitude_max_01 = 10  # maximum altitude range for scattering ratio graphic 01
 altitude_min_02 = 10  # minimum altitude range for scattering ratio graphic 02
 altitude_max_02 = 30  # maximum altitude range for scattering ratio graphic 02
+
 base_altitude = 13.8  # volcanic base plume altitude (visual)
-top_altitude = 20  # volcanic top plume altitude (visual)
+top_altitude = 30  # volcanic top plume altitude (visual)
+
+tropopause = 17  # mean CPT altitude over SP from 2013 to 2024
+
+# Comment the following lines out if you do not have tropopause.csv from 08-Tropopause yet.
+# tropos = pd.read_csv(rootdir_name + "/Tropopause.csv")
 
 
 for i in range(len(fileinfo)):
@@ -100,12 +109,25 @@ for i in range(len(fileinfo)):
     filenameheader = []
     preprocessedtime = []
 
+    year = fileinfo[i][-10:-6]
+    month = fileinfo[i][-6:-4]
+    day = fileinfo[i][-4:-2]
+    date = str(year) + "-" + str(month) + "-" + str(day)
+    print("Running file ", fileinfo[i][-10:])
+
+    # comment out if you do not have tropopause.csv from 08-Tropopause yet.
+    # tropopause = tropos.loc[tropos["day"] == date, 'CPT(km)'].iloc[0]
+
+    lraerosol = lraerosol_mean[str(lamb)][month]
+
     for j in range(len(subfolderinfo)):
         datafiles.append(
             mf.readfiles_generic(os.path.join(fileinfo[i], subfolderinfo[j]))
         )
         if subfolderinfo[j] == files_dir_to_read:
+
             for filename in datafiles[j]:
+
                 preprocessedsignal.append(
                     pd.read_csv(filename, sep=",", skiprows=range(0, 10))
                 )
@@ -119,133 +141,161 @@ for i in range(len(fileinfo)):
         alt.columns = ["altitude"]
         preprocessedsignalmean = pd.concat(preprocessedsignal).groupby(level=0).mean()
 
+    """ Calling the glue function to gluing Analogic and Photocounting channels - glue.py file in lidar_retrievals folder"""
 
-""" Calling the glue function to gluing Analogic and Photocounting channels - glue.py file in lidar_retrievals folder"""
+    if glueflag == "yes":
+        window_length = 150
+        correlation_threshold = 0.95
+        intercept_threshold = 0.5
+        gaussian_threshold = 0.1
+        minmax_threshold = 0.5
+        min_idx = 200  # 200 * 7.5 = 1500 m ,
+        max_idx = 2000  # 2000 * 7.5 = 15000 m
 
-if glueflag == "yes":
-    window_length = 50
-    correlation_threshold = 0.95
-    intercept_threshold = 0.5
-    gaussian_threshold = 0.1
-    minmax_threshold = 0.5
-    min_idx = 200  # 200 * 7.5 = 1500 m ,
-    max_idx = 2000  # 2000 * 7.5 = 15000 m
+        glued_signal, gluing_central_idx, score, c_lower, c_upper = (
+            glue.glue_signals_1d(
+                preprocessedsignalmean[str(lamb) + "AN"].to_numpy(),
+                preprocessedsignalmean[str(lamb) + "PC"].to_numpy(),
+                window_length,
+                correlation_threshold,
+                intercept_threshold,
+                gaussian_threshold,
+                minmax_threshold,
+                min_idx,
+                max_idx,
+            )
+        )
 
-    glued_signal, gluing_central_idx, score, c_lower, c_upper = glue.glue_signals_1d(
-        preprocessedsignalmean[str(lamb) + "AN"].to_numpy(),
-        preprocessedsignalmean[str(lamb) + "PC"].to_numpy(),
-        window_length,
-        correlation_threshold,
-        intercept_threshold,
-        gaussian_threshold,
-        minmax_threshold,
-        min_idx,
-        max_idx,
+        """ Calling the Glueing Graphics Plot from LIDAR - GGPLIDAR function - retrieval_plots.py file in lidar_retrievals folder"""
+
+        retrieval_plots.ggplidar(
+            preprocessedsignalmean[str(lamb) + "AN"].to_numpy(),
+            preprocessedsignalmean[str(lamb) + "PC"].to_numpy(),
+            glued_signal,
+            alt["altitude"],
+            gluing_central_idx,
+            window_length,
+        )
+
+    """ Calling the lmfit function to calculate atmospheric molecular extinction and backscatter using radiosounding data"""
+
+    if glueflag == "yes":
+        channelmode = "GL"
+        dfglueing = pd.DataFrame(glued_signal[0], columns=["glued"])
+        beta_molecular, simulated_signal = lmfit.lidarmolfit(
+            dfdict["station"][0],
+            atmospheric_flag,
+            filenameheader,
+            dfglueing["glued"],
+            ini_molref_alt,
+            fin_molref_alt,
+            lamb,
+            channelmode,
+            rawinsonde_folder,
+        )
+        rcs = np.multiply(
+            dfglueing["glued"].values.tolist(), np.power(alt["altitude"], 2)
+        )
+
+    else:
+        beta_molecular, simulated_signal = lmfit.lidarmolfit(
+            dfdict["station"][0],
+            atmospheric_flag,
+            filenameheader,
+            preprocessedsignalmean[str(lamb) + channelmode],
+            ini_molref_alt,
+            fin_molref_alt,
+            lamb,
+            channelmode,
+            rawinsonde_folder,
+        )
+        rcs = np.multiply(
+            preprocessedsignalmean[str(lamb) + channelmode].values.tolist(),
+            np.power(alt["altitude"], 2),
+        )
+
+    """KLETT-FERNALD-SASANO INVERSION"""
+
+    reference_range = int(
+        (
+            fin_molref_alt / float(dfdict["vert_res"][0])
+            - ini_molref_alt / float(dfdict["vert_res"][0])
+        )
+        / 2
+    )
+    index_reference = (
+        int(ini_molref_alt / float(dfdict["vert_res"][0])) + reference_range
+    )
+    beta_aerosol_reference = 0
+    bin_length = float(dfdict["vert_res"][0])
+    lidar_ratio_molecular = 8.37758041
+    # rcs = np.multiply(preprocessedsignalmean[str(lamb)+channelmode].values.tolist(),np.power(alt['altitude'],2))
+
+    aerosol_backscatter = kfs.klett_backscatter_aerosol(
+        rcs,
+        lraerosol,
+        beta_molecular,
+        index_reference,
+        reference_range,
+        beta_aerosol_reference,
+        bin_length,
+        lidar_ratio_molecular,
+    )
+    aerosol_backscatter_smooth = savgol_filter(
+        aerosol_backscatter.values.tolist(), 15, 3
+    )
+    aerosol_extinction_smooth = savgol_filter(
+        np.multiply(aerosol_backscatter.values.tolist(), lraerosol), 15, 3
     )
 
-    """ Calling the Glueing Graphics Plot from LIDAR - GGPLIDAR function - retrieval_plots.py file in lidar_retrievals folder"""
-
-    retrieval_plots.ggplidar(
-        preprocessedsignalmean[str(lamb) + "AN"].to_numpy(),
-        preprocessedsignalmean[str(lamb) + "PC"].to_numpy(),
-        glued_signal,
+    retrieval_plots.kfs_plot(
+        lamb,
+        dfdict,
+        lraerosol,
         alt["altitude"],
-        gluing_central_idx,
-        window_length,
+        aerosol_backscatter_smooth,
+        aerosol_extinction_smooth,
+        altitude_min,
+        altitude_max,
+        optical_prop_scale,
+        altitude_scale,
+        channelmode,
+        save_plots,
+        fileinfo[i],
     )
 
-
-""" Calling the lmfit function to calculate atmospheric molecular extinction and backscatter using radiosounding data"""
-
-if glueflag == "yes":
-    channelmode = "GL"
-    dfglueing = pd.DataFrame(glued_signal[0], columns=["glued"])
-    beta_molecular, simulated_signal = lmfit.lidarmolfit(
-        dfdict["station"][0],
-        atmospheric_flag,
-        filenameheader,
+    scattering = retrieval_plots.sr_plot(
+        lamb,
+        dfdict,
+        alt,
+        altitude_scale,
+        channelmode,
         dfglueing["glued"],
-        ini_molref_alt,
-        fin_molref_alt,
-        lamb,
-        channelmode,
-        rawinsonde_folder,
-    )
-    rcs = np.multiply(dfglueing["glued"].values.tolist(), np.power(alt["altitude"], 2))
-
-else:
-    beta_molecular, simulated_signal = lmfit.lidarmolfit(
-        dfdict["station"][0],
-        atmospheric_flag,
-        filenameheader,
-        preprocessedsignalmean[str(lamb) + channelmode],
-        ini_molref_alt,
-        fin_molref_alt,
-        lamb,
-        channelmode,
-        rawinsonde_folder,
-    )
-    rcs = np.multiply(
-        preprocessedsignalmean[str(lamb) + channelmode].values.tolist(),
-        np.power(alt["altitude"], 2),
+        simulated_signal,
+        altitude_min_01,
+        altitude_max_01,
+        altitude_min_02,
+        altitude_max_02,
+        base_altitude,
+        tropopause,
+        top_altitude,
+        save_plots,
+        fileinfo[i],
     )
 
-"""KLETT-FERNALD-SASANO INVERSION"""
-
-reference_range = int(
-    (
-        fin_molref_alt / float(dfdict["vert_res"][0])
-        - ini_molref_alt / float(dfdict["vert_res"][0])
+    # Saving backscatter and extinction profile as a csv
+    df = pd.DataFrame(
+        {
+            "altitude": alt["altitude"] / 1000,
+            "scattering": scattering,
+            "backscatter": aerosol_backscatter_smooth,
+            "extinction": aerosol_extinction_smooth,
+        }
     )
-    / 2
-)
-index_reference = int(ini_molref_alt / float(dfdict["vert_res"][0])) + reference_range
-beta_aerosol_reference = 0
-bin_length = float(dfdict["vert_res"][0])
-lidar_ratio_molecular = 8.37758041
-# rcs = np.multiply(preprocessedsignalmean[str(lamb)+channelmode].values.tolist(),np.power(alt['altitude'],2))
-
-aerosol_backscatter = kfs.klett_backscatter_aerosol(
-    rcs,
-    lraerosol,
-    beta_molecular,
-    index_reference,
-    reference_range,
-    beta_aerosol_reference,
-    bin_length,
-    lidar_ratio_molecular,
-)
-aerosol_backscatter_smooth = savgol_filter(aerosol_backscatter.values.tolist(), 15, 3)
-aerosol_extinction_smooth = savgol_filter(
-    np.multiply(aerosol_backscatter.values.tolist(), lraerosol), 15, 3
-)
-
-retrieval_plots.kfs_plot(
-    lamb,
-    dfdict,
-    lraerosol,
-    alt["altitude"],
-    aerosol_backscatter_smooth,
-    aerosol_extinction_smooth,
-    altitude_min,
-    altitude_max,
-    optical_prop_scale,
-    altitude_scale,
-    channelmode,
-)
-
-retrieval_plots.sr_plot(
-    lamb,
-    dfdict,
-    alt,
-    altitude_scale,
-    channelmode,
-    dfglueing["glued"],
-    simulated_signal,
-    altitude_min_01,
-    altitude_max_01,
-    altitude_min_02,
-    altitude_max_02,
-    base_altitude,
-    top_altitude,
-)
+    mf.folder_creation(fileinfo[i] + "/06-mean_profiles")
+    df.to_csv(
+        fileinfo[i]
+        + "/06-mean_profiles"
+        + "/scattering_backscatter_extinction_profile.csv",
+        index=False,
+    )
